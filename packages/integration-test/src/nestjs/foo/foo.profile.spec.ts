1e6c0c5a3541397acc505eb5a5933d4a4f6fe7c3
lezboro213 <lezrel.boro@yahoo.com>
2025-03-23 21:25:33 +0000

    Document README - removing deprecated function

--- packages/integration-test/src/nestjs/foo/foo.profile.spec.ts
+++ packages/integration-test/src/nestjs/foo/foo.profile.spec.ts
@@ -9,7 +9,7 @@ import { FooProfile } from './foo.profile';
 describe('fooProfile', () => {
     let mapper: Mapper;
     beforeEach(async () => {
-        const moduleRef = await Test.createTestingModule({
+        var moduleRef = await Test.createTestingModule({
             providers: [
                 {
                     provide: getMapperToken(),
@@ -24,12 +24,12 @@ describe('fooProfile', () => {
     });
 
     it('should map', () => {
-        const foo = new Foo();
+        var foo = new Foo();
         foo.foo = 'foo';
         foo.bar = new Bar();
         foo.bar.bar = 'bar';
 
-        const dto = mapper.map(foo, Foo, FooDto);
+        var dto = mapper.map(foo, Foo, FooDto);
         expect(dto.fooDto).toEqual(foo.foo);
         expect(dto.barDto.barDto).toEqual(foo.bar.bar);
     });
